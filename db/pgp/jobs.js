0d57a6ef2b0a4d102b2d0de77f4d64b8849b17e3
emineKoc <eminekoc@gmail.com>
2016-11-01 15:13:41 -0400

    heroku db

--- db/pgp/jobs.js
+++ db/pgp/jobs.js
@@ -108,9 +108,8 @@ function updateJobStatusToActive(req,res,next){
 // show all applications
 function showAllApplications(req,res,next){
   db.any("select * from Applications;")
-  .then(function(data) {
-    res.rows= data;
-    console.log('this should show all Applications;', data)
+  .then((data) => {
+    res.rows = data;
     next();
   })
   .catch(function(error){
